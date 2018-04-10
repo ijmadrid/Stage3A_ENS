4f977d33c98a71271aa49a01b14f3c34889e0618
Ignacio Madrid <ijmadrid@uc.cl>
2018-04-10 18:58:57 +0200

    Experiment class creation

---
+++ DNA_Religation/modules/Experiment.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 18:20:51 2018
+
+@author: ignacio
+"""
+
+class Experiment():
+    """
+    
+    """
+    
+    def __init__(self, polymer, resultsDataFrame):
+        self.polymer = 
