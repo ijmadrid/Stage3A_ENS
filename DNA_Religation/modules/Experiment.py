Fixed numerical problems with dt, making figures

DNA_Religation/modules/Experiment.py
@@ -10,10 +10,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 from collections import Counter
-import multiprocessing as mp
 from .Forces import ExcludedVolume, LocalExcludedVolume
 from .Polymers import RCLPolymer
-import pandas as pd
 from time import time
 
 class Experiment():
@@ -237,7 +235,7 @@ class Experiment():
         self.addResults("iterationsNumber",self.numRealisations)
         FETs = []
         events = []
-        repairProba = []
+        post_msrgs = []
         
         for i in range(self.numRealisations):
             
@@ -253,27 +251,43 @@ class Experiment():
             if( t <self.numMaxSteps):
                 FETs.append(t)
                 events.append(self.polymer.anyEncountered(self.encounterDistance)[1])
+
+                msrg_post_encounter = self.polymer.get_msrg()
+                
+#                pre_msrgs.append(msrg_pre_encounter)
+                post_msrgs.append(msrg_post_encounter)
         
             self.polymer = self.polymer.new()
 
         
         # Prepare results 
         FETs = np.array(FETs)*self.dt
+        self.addResults("events",events)
         events = Counter(events)
         total_valid_experiments = sum(events.values())
         
         if total_valid_experiments == 0:
-            repairProba = [0.5,0.5]
+            repairProbas = 0.
+            repairHalfCI = 0.
+            mFET = np.nan
+            halfCI_mFET = 0.
             print('No valid experiments!')
         else:
             proba = events['Repair']/total_valid_experiments
-            repairProba = [proba,
-                           1.96*np.sqrt((proba - proba**2)/total_valid_experiments)]
-        
+            repairProbas = proba
+            repairHalfCI = 1.96*np.sqrt((proba - proba**2)/total_valid_experiments)
+            mFET = np.mean(FETs)
+            halfCI_mFET = 1.96*np.std(FETs)/np.sqrt(total_valid_experiments)
+            
         # Save results
         self.addResults("FETs",FETs)
+        self.addResults("meanFET",mFET)
+        self.addResults("halfCI_FET",halfCI_mFET)
         self.addResults("eventsCounter",events)
-        self.addResults("repair_probability_CI",repairProba)
+        self.addResults("repair_probability",repairProbas)
+        self.addResults("repair_CIhalflength",repairHalfCI)
+        
+        self.addResults('MSRG_atEncounter', post_msrgs)
     
     
     
@@ -285,8 +299,7 @@ class Experiment():
         self.addResults("iterationsNumber",self.numRealisations)
         FETs = []
         events = []
-        repairProba = []
-        pre_msrgs = []
+#        pre_msrgs = []
         post_msrgs = []
 
         if self.numRealisations >= 10: k = self.numRealisations//10        
@@ -313,7 +326,7 @@ class Experiment():
             self.polymer.step(self.waitingSteps,self.dt_relax,self.diffusionConstant)
     
             # Measure MSRG after the break
-            msrg_pre_encounter = self.polymer.get_msrg()
+#            msrg_pre_encounter = self.polymer.get_msrg()
         
             
             # Simulation until encounter              
@@ -328,7 +341,7 @@ class Experiment():
                 
                 msrg_post_encounter = self.polymer.get_msrg()
                 
-                pre_msrgs.append(msrg_pre_encounter)
+#                pre_msrgs.append(msrg_pre_encounter)
                 post_msrgs.append(msrg_post_encounter)
                 
                 
@@ -337,33 +350,37 @@ class Experiment():
 
         
         # Prepare results 
-        data = pd.DataFrame()
+#        data = pd.DataFrame()
         FETs = np.array(FETs)*self.dt
-        data['event'] = events
+#        data['event'] = events
+        self.addResults("events",events)
         events = Counter(events)
         total_valid_experiments = sum(events.values())
         
         if total_valid_experiments == 0:
-            repairProba = [0.5,0.5]
+            repairProbas = 0.
+            repairHalfCI = 0.
+            mFET = np.nan
+            halfCI_mFET = 0.
             print('No valid experiments!')
         else:
             proba = events['Repair']/total_valid_experiments
-            repairProba = [proba,
-                           1.96*np.sqrt((proba - proba**2)/total_valid_experiments)]
-        
+            repairProbas = proba
+            repairHalfCI = 1.96*np.sqrt((proba - proba**2)/total_valid_experiments)
+            mFET = np.mean(FETs)
+            halfCI_mFET = 1.96*np.std(FETs)/np.sqrt(total_valid_experiments)
+            
         # Save results
-        
-        
-
-        data['FET'] = FETs
-        data['MSRG_pre_encounter'] = pre_msrgs
-        data['MSRG_encounter'] = post_msrgs
-
-        self.addResults("polymerParams",self.polymer.get_params())
         self.addResults("FETs",FETs)
+        self.addResults("meanFET",mFET)
+        self.addResults("halfCI_FET",halfCI_mFET)
         self.addResults("eventsCounter",events)
-        self.addResults("repair_probability_CI",repairProba)
-        self.addResults("results_dataframe", data)
+        self.addResults("repair_probability",repairProbas)
+        self.addResults("repair_CIhalflength",repairHalfCI)
+
+#        self.addResults('MSRG_pre_encounter', pre_msrgs)
+        self.addResults('MSRG_atEncounter', post_msrgs)
+#        self.addResults("results_dataframe", data)
 
 
     def TAD_repair(self):
@@ -371,7 +388,6 @@ class Experiment():
         self.addResults("iterationsNumber",self.numRealisations)
         FETs = []
         events = []
-        repairProba = []
         
         for i in range(self.numRealisations):
             
@@ -423,17 +439,25 @@ class Experiment():
         total_valid_experiments = sum(events.values())
         
         if total_valid_experiments == 0:
-            repairProba = [0.5,0.5]
+            repairProbas = 0.
+            repairHalfCI = 0.
+            mFET = np.nan
+            halfCI_mFET = 0.
             print('No valid experiments!')
         else:
             proba = events['Repair']/total_valid_experiments
-            repairProba = [proba,
-                           1.96*np.sqrt((proba - proba**2)/total_valid_experiments)]
-        
+            repairProbas = proba
+            repairHalfCI = 1.96*np.sqrt((proba - proba**2)/total_valid_experiments)
+            mFET = np.mean(FETs)
+            halfCI_mFET = 1.96*np.std(FETs)/np.sqrt(total_valid_experiments)
+            
         # Save results
         self.addResults("FETs",FETs)
+        self.addResults("meanFET",mFET)
+        self.addResults("halfCI_FET",halfCI_mFET)
         self.addResults("eventsCounter",events)
-        self.addResults("repair_probability_CI",repairProba)
+        self.addResults("repair_probability",repairProbas)
+        self.addResults("repair_CIhalflength",repairHalfCI)
     
     
     def oneTADrepair(self):
@@ -444,7 +468,6 @@ class Experiment():
         self.addResults("iterationsNumber",self.numRealisations)
         FETs = []
         events = []
-        repairProba = []
         boundaries = np.append(0, self.polymer.subDomainnumMonomers.cumsum())
         k = self.selectedSubDomain
         l = boundaries[k]
@@ -506,16 +529,22 @@ class Experiment():
         total_valid_experiments = sum(events.values())
         
         if total_valid_experiments == 0:
-            repairProba = [0.5,0.5]
+            repairProbas = 0.
+            repairHalfCI = 0.
+            mFET = np.nan
+            halfCI_mFET = 0.
             print('No valid experiments!')
         else:
             proba = events['Repair']/total_valid_experiments
-            repairProba = [proba,
-                           1.96*np.sqrt((proba - proba**2)/total_valid_experiments)]
+            repairProbas = proba
+            repairHalfCI = 1.96*np.sqrt((proba - proba**2)/total_valid_experiments)
+            mFET = np.mean(FETs)
+            halfCI_mFET = 1.96*np.std(FETs)/np.sqrt(total_valid_experiments)
         
         # Save results
         self.addResults("FETs",FETs)
+        self.addResults("meanFET",mFET)
+        self.addResults("halfCI_FET",halfCI_mFET)
         self.addResults("eventsCounter",events)
-        self.addResults("repair_probability_CI",repairProba)
-        
-        return 
+        self.addResults("repair_probability",repairProbas)
+        self.addResults("repair_CIhalflength",repairHalfCI)

DNA_Religation/projects/RCLPolymer_Repair_Probability/repairProbability_v_NumberOfConectors.py
@@ -15,6 +15,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
 from modules.Polymers import RCLPolymer
 from modules.Experiment import Experiment
@@ -29,10 +30,10 @@ from pathos.multiprocessing import Pool
 
 p0 = RCLPolymer(100, 3, 0.2, 3)
 
-params = dict(dt_relax = 0.5,
+params = dict(dt_relax = 0.01,
           diffusionConstant = 0.008,
 #              numSteps = 100,
-          dt = 0.1,
+          dt = 0.01,
           genomicDistance = 2,
           Nb = 2,
           waitingSteps = 200,
@@ -46,7 +47,7 @@ params['numRealisations'] = numRealisations
 
 
 test_genomic_distances = [3,5,10]
-x_Nc = np.arange(3,20,2)
+x_Nc = np.arange(3,30,2)
 
 #############################################################
 ############ FUNCTIONS ######################################
@@ -110,32 +111,34 @@ def proba_vs_conectorsNumber(test_genomic_distances,x_Nc,keepCL,errorbars=False)
 
 
 def measureProba(g):
-    probas = []
+    probas = np.zeros(len(x_Nc))
+    halfCI = np.zeros(len(x_Nc))
     params['genomicDistance'] = g
     
-    for Nc in x_Nc:            
+    for i, Nc in enumerate(x_Nc):            
         results = {}
         np.random.seed()
         p0 = RCLPolymer(100, 3, 0.2, Nc, False)
         mc = Experiment(p0, results, params,"EncounterSimulation") 
-        probas.append(mc.results['repair_probability_CI'][0])
-   
-    return probas
+        probas[i] = mc.results['repair_probability']
+        halfCI[i] = mc.results['repair_CIhalflength']
+    return (probas, halfCI)
 
 
 if __name__ == '__main__':
     
-    print("::::::: SERIAL EXPERIMENTS :::::::::::")
-    
-    verystart = time()
-    
-    proba_vs_conectorsNumber(test_genomic_distances,x_Nc,False,errorbars=True)
-
-    print("Total duration : ", time()-verystart)
+#    print("::::::: SERIAL EXPERIMENTS :::::::::::")
+#    
+#    verystart = time()
+#    
+#    proba_vs_conectorsNumber(test_genomic_distances,x_Nc,False,errorbars=True)
+#
+#    print("Total duration : ", time()-verystart)
     
     print("::::::: PARALLEL EXPERIMENTS :::::::::")
     
     plt.figure()
+    rcParams.update({'axes.labelsize': 'xx-large'})
     
     verystart = time()
     workers = len(test_genomic_distances)
@@ -145,7 +148,7 @@ if __name__ == '__main__':
     res = pool.map(measureProba, test_genomic_distances)
     
     for i in range(len(res)):
-        plt.plot(x_Nc,res[i],'-o',label=r'$g = $ '+str(test_genomic_distances[i]))
+        plt.plot(x_Nc,res[i][0],'-o',label=r'$g = $ '+str(test_genomic_distances[i]))
     
     plt.legend()
     plt.show()    

DNA_Religation/projects/RCLPolymer_TwoDomains/SemirandomConnections.py
@@ -23,6 +23,7 @@ from matplotlib import rcParams
 from time import strftime
 import scipy.stats as sts
 import pickle
+import csv
 
 ############################################################################
 ############################################################################
@@ -39,31 +40,33 @@ NcMatrix[1,1] = 0
 polymerParams = dict(numMonomers = 100, # TODO (.., ..., ...)
                      dim         = 3,
                      b           = 0.2,
-                     Nc          = 25,
-                     keepCL      = True
+                     Nc          = 15,
+                     keepCL      = False
                      )
 
 simulationParams = dict(# Physicial parameters
                         diffusionConstant = 0.008,
                         # Numerical parameters
-                        numRealisations   = 600, 
+                        numRealisations   = 500, 
                         dt                = 0.01,
                         dt_relax          = 0.01,
 #                        numSteps          = 500,
-#                        excludedVolumeCutOff = 0.2,
+                        excludedVolumeCutOff = 0.1,
                         waitingSteps = 200,
-                        numMaxSteps = 500,
+                        numMaxSteps = 1000,
                         encounterDistance = 0.1,
                         genomicDistance = 10,
-                        Nb = 2,
-                        selectedSubDomain = 0
+                        Nb = 2
+#                        selectedSubDomain = 0
                         )
 
 test_distances = np.arange(1,30,3,dtype = int)
 
-gmax = 40
+gmax = 20
 gStep = 1
 test_epsilons = [0.1]
+
+x_Nc = np.arange(3,30,2)
 
 errorbars = True
 
@@ -243,7 +246,7 @@ def FET_Simulation(polymerParams,simulationParams):
     
     p0 = RCLPolymer(**polymerParams)
     results = {}
-    mc = Experiment(p0, results, simulationParams,"oneTAD_Repair")
+    mc = Experiment(p0, results, simulationParams,"EncounterSimulation")
      
     plotFET(mc)
     
@@ -263,71 +266,202 @@ def FET_Simulation(polymerParams,simulationParams):
 
 
 
-def proba_vs_genomicDistance(polymerParams,simulationParams,gmax,gStep,errorbars=False):
-#    gmax = nb_monomers - 3
-    gmin = 2
-    
-    probas = []
-    demiCI = []
-    
-    plt.figure()
-    rcParams.update({'axes.labelsize': 'xx-large'})
-    plt.xlabel('genomic distance (in number of monomers)')
-    plt.ylabel(r'$\mathbb{P}$(Repair)')
-    
-    xg = np.arange(gmin,gmax,gStep,dtype=int)
-    
+def proba_vs_genomicDistance(experimentName,polymerParams,simulationParams,gmax,gStep,errorbars=False):
+
     date = strftime("%Y_%m_%d_%H_%M")
+    filename = date + '_proba_vs_genomicDistance_' + experimentName + '.csv'
     
-    output = np.empty((2,3,len(xg)))
-    for i, keepCL in enumerate([True,False]):
+    with open('results/'+filename, 'w') as csvfile:
         
-        if keepCL:
-            labelkeep = 'Keeping CL in damage zone'
-        else:
-            labelkeep = 'Removing CL in damage zone'
+        gmin = 2
+          
+        plt.figure()
+        rcParams.update({'axes.labelsize': 'xx-large'})
+        plt.xlabel('genomic distance (in number of monomers)')
+        plt.ylabel(r'$\mathbb{P}$(Repair)')
         
-        polymerParams['keepCL'] = keepCL
+        xg = np.arange(gmin,gmax,gStep,dtype=int)
         
-        probas = []
-        demiCI = []
-        for g in xg:    
-            print("Simulation for g =",g,"and ",labelkeep)
-            p0 = RCLPolymer(**polymerParams)
-            results = {}
-            simulationParams['genomicDistance'] = g
-            mc = Experiment(p0, results, simulationParams,"EncounterSimulation")
-#            oneTAD_Repair
-            probas.append(mc.results['repair_probability_CI'][0])
-            demiCI.append(mc.results['repair_probability_CI'][1])
+        for i, keepCL in enumerate([True,False]):
+            
+            if keepCL:
+                labelkeep = 'Keeping CL in damage zone'
+            else:
+                labelkeep = 'Removing CL in damage zone'
+            
+            polymerParams['keepCL'] = keepCL
+            
+            probas = np.zeros(len(xg))
+            demiCI = np.zeros(len(xg))
+            
+            for j, g in enumerate(xg):    
+                print("Simulation for g =",g,"and ",labelkeep)
+                p0 = RCLPolymer(**polymerParams)
+                simulationParams['genomicDistance'] = g
+                results = {**polymerParams, **simulationParams}
+                mc = Experiment(p0, results, simulationParams,experimentName)
+    #            oneTAD_Repair
+                probas[j] = mc.results['repair_probability']
+                demiCI[j] = mc.results['repair_CIhalflength']
+                
+                if i == 0 and g == gmin:
+                    fieldnames = ['experimentSetID']+list(mc.results)
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                writer.writerow({**{'experimentSetID' : str(i)}, **mc.results})
+            
+            
+            probas = np.array(probas)
+            demiCI = np.array(demiCI)
+            
+            if errorbars:
+                plt.errorbar(x=xg, y=probas, yerr=demiCI,
+                             fmt='-o', label=labelkeep, capsize = 4)
+            else:
+                plt.plot(xg,probas,'-o',label=labelkeep)
         
-        probas = np.array(probas)
-        demiCI = np.array(demiCI)
-        
-        output[i][0] = xg
-        output[i][1] = probas
-        output[i][2] = demiCI
-        
-        if errorbars:
-            plt.errorbar(x=xg, y=probas, yerr=demiCI,
-                         fmt='-o', label=labelkeep, capsize = 4)
-        else:
-            plt.plot(xg,probas,'-o',label=labelkeep)
+        plt.legend()        
+        plt.show()
 
-    np.save('results/proba_vs_genomicDistance_keep_and_remove__'+date+'.npy',output)
+
+def proba_vs_genomicDistance_andVE(polymerParams,simulationParams,gmax,gStep,errorbars=False):
+
+    date = strftime("%Y_%m_%d_%H_%M")
+    filename = date + '_VE_proba_vs_genomicDistance_' + '.csv'
     
-    plt.legend()        
-    plt.show()
+    with open('results/'+filename, 'w') as csvfile:
+        
+        gmin = 2
+        
+        
+        plt.figure()
+        rcParams.update({'axes.labelsize': 'xx-large'})
+        plt.xlabel('genomic distance (in number of monomers)')
+        plt.ylabel(r'$\mathbb{P}$(Repair)')
+        
+        xg = np.arange(gmin,gmax,gStep,dtype=int)
+        
+        for i, VolumeExclusion in enumerate([True,False]):
+            
+            if VolumeExclusion:
+                labelkeep = 'Excluding volume with a cutoff of ' + str(simulationParams['excludedVolumeCutOff']) + ' μm'
+                experimentName = "Encounter_withExcludedVolume"
+            else:
+                labelkeep = 'Without excluded volume'
+                experimentName = "EncounterSimulation"        
+                simulationParams['excludedVolumeCutOff'] = 0
+            probas = np.zeros(len(xg))
+            demiCI = np.zeros(len(xg))
+            for j, g in enumerate(xg):    
+                print("Simulation for g =",g,"and ",labelkeep)
+                p0 = RCLPolymer(**polymerParams)
+                simulationParams['genomicDistance'] = g
+                results = {**polymerParams, **simulationParams}
+                mc = Experiment(p0, results, simulationParams,experimentName)
+    #            oneTAD_Repair
+                probas[j] = mc.results['repair_probability']
+                demiCI[j] = mc.results['repair_CIhalflength']
+                
+                if i == 0 and g == gmin:
+                    fieldnames = ['experimentSetID']+list(mc.results)
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                writer.writerow({**{'experimentSetID' : str(i)+'_'+str(j)}, **mc.results})
+            
+            if errorbars:
+                plt.errorbar(x=xg, y=probas, yerr=demiCI,
+                             fmt='-o', label=labelkeep, capsize = 4)
+            else:
+                plt.plot(xg,probas,'-o',label=labelkeep)
+        
+        plt.legend()        
+        plt.show()
+
+
+def proba_vs_Nc_andVE(polymerParams,simulationParams,x_Nc,errorbars=False):
+
+    date = strftime("%Y_%m_%d_%H_%M")
+    filename = date + '_VE_proba_vs_Nc' + '.csv'
     
+    with open('results/'+filename, 'w') as csvfile:
+                
+        plt.figure()
+        rcParams.update({'axes.labelsize': 'xx-large'})
+        rcParams.update({'legend.fontsize': 'large'})
+        plt.xlabel('genomic distance (in number of monomers)')
+        plt.ylabel(r'$\mathbb{P}$(Repair)')
+        
+        first_time = True
+        for i, VolumeExclusion in enumerate([True,False]):
+            
+            if VolumeExclusion:
+                labelkeep = 'Excluding volume with a cutoff of ' + str(simulationParams['excludedVolumeCutOff']) + ' μm'
+                experimentName = "Encounter_withExcludedVolume"
+            else:
+                labelkeep = 'Without excluded volume'
+                experimentName = "EncounterSimulation"        
+                simulationParams['excludedVolumeCutOff'] = 0
+            probas = np.zeros(len(x_Nc))
+            demiCI = np.zeros(len(x_Nc))
+            for j, Nc in enumerate(x_Nc):    
+                print("Simulation for Nc =",Nc,"and",labelkeep)
+                polymerParams['Nc'] = Nc
+                p0 = RCLPolymer(**polymerParams)
+                results = {**polymerParams, **simulationParams}
+                mc = Experiment(p0, results, simulationParams,experimentName)
+    #            oneTAD_Repair
+                probas[j] = mc.results['repair_probability']
+                demiCI[j] = mc.results['repair_CIhalflength']
+                
+                if first_time:
+                    fieldnames = ['experimentSetID']+list(mc.results)
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    first_time = False
+                writer.writerow({**{'experimentSetID' : str(i)+'_'+str(j)}, **mc.results})
+            
+            if errorbars:
+                plt.errorbar(x=x_Nc, y=probas, yerr=demiCI,
+                             fmt='-o', label=labelkeep, capsize = 4)
+            else:
+                plt.plot(x_Nc,probas,'-o',label=labelkeep)
+        
+        plt.legend()        
+        plt.show()
 
-
+def opencsv(file,wantedResults):
+    wantedResults = {res : [] for res in wantedResults}
+    with open(file, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            for res in wantedResults:
+                try:
+                    wantedResults[res].append(float(row[res]))
+                except:
+                    wantedResults[res].append(row[res])
+    return wantedResults
+            
 
 if __name__ == "__main__":
+    
+    file = 'results/2018_04_20_12_55_VE_proba_vs_genomicDistance_.csv'
+    wanted = {'genomicDistance',
+              'excludedVolumeCutOff',
+              'repair_probability',
+              'repair_CIhalflength',
+              'FETs'}
+    res = opencsv(file,wanted)
+
+    a = [row.split(' ')[1:] for row in res['FETs'][0].split('\n')]
+    b = [s for r in a for s in r]
+
+    
     
 #    mc = proba_v_interDomainDistance(polymerParams, simulationParams, TAD_size, test_distances, errorbars)
 #    mc = proba_v_connectivityFraction(polymerParams, simulationParams, test_xi, test_TADsize, errorbars)
 #    mc = FET_Simulation(polymerParams,simulationParams)
 
-    proba_vs_genomicDistance(polymerParams,simulationParams,gmax,gStep,errorbars)
-
+#    proba_vs_genomicDistance(polymerParams,simulationParams,gmax,gStep,errorbars)
+#    proba_vs_genomicDistance_andVE(polymerParams,simulationParams,gmax,gStep,errorbars)
+#    proba_vs_Nc_andVE(polymerParams,simulationParams,x_Nc,errorbars)
 #    print(__name__)